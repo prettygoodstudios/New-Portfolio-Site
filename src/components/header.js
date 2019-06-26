17f8b00485a5ab824b02328ac1553d51213d66a5
Miguel Rust <hava12soft@gmail.com>
2019-06-26 12:02:37 -0600

    Built out header layout.

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,53 @@
 import React, {Component} from "react";
 
 class Header extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            toggleState: "open"
+        }
+        window.onresize = () => this.updateHeader();
+    }
+
+    componentWillUnMount(){
+
+    }
+
+    updateHeader = () => {
+        console.log("Updat")
+        if(window.innerWidth > 600 && this.state.toggleState == "close"){
+            this.setState({
+                toggleState: "open"
+            });
+        }
+    }
+
+    toggleMenu = () => {
+        this.setState({
+            toggleState: this.state.toggleState === "open" ? "close" : "open"
+        });
+    }
+
     render(){
+        const {toggleState} = this.state;
         return(
             <div className="header">
                 <div className="header__title">
                     <h1>Miguel Rust</h1>
                 </div>
-                <div className="header__menu">
+                <div className={"header__menu "+((toggleState == "open") ? "close-menu" : "open-menu")}>
                     <a>Home</a>
                     <a>About</a>
                     <a>Portfolio</a>
                     <a>Contact</a>
                 </div>
                 <div className="header__toggle">
-                    <button>Menu</button>
+                    <button onClick={this.toggleMenu} className={toggleState}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
                 </div>
             </div>
         );
