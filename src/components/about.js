78254e423315bbf6399ba79832c506527263ccab
Miguel Rust <hava12soft@gmail.com>
2019-07-09 22:09:38 -0600

    Built out job history timeline.

--- src/components/about.js
+++ src/components/about.js
@@ -18,69 +18,54 @@ const About = (props) => {
             </div>
             <div className="job-section">
                 <h3>Job History</h3>
-                <p>References can be made available upon request. Hover or tap on each item to learn more.</p>
-                <div className="job-section__jobs">
-                    <div className="job-section__jobs__item">
-                        <div>
-                            <h5>Front End Web Development Intern</h5>
-                            <h6>Namify LLC</h6>
-                            <h6><i class="fas fa-calendar-day"></i>January 2018 to May 2018</h6>
-                            <p>I was a Frontend Web Developement Intern at Namify from January 2018 to May 2018. I worked on the Axomo eCommerce platform while there. I gained experience with the following technologies during my time at Namify AngularJS, .NET, and Microsoft SQL Server.</p>
-                        </div>
-                        <img src="https://namify.com/wp-content/uploads/2017/06/Namify_Logo_Color-300x132.png"/>
-                    </div>
-                    <div className="job-section__jobs__item">
-                        <div>
-                            <h5>Quality Insurance Analyst</h5>
-                            <h6>Namify LLC</h6>
-                            <h6><i class="fas fa-calendar-day"></i>January 2018 to May 2018</h6>
-                            <p>I was a Frontend Web Developement Intern at Namify from January 2018 to May 2018. I worked on the Axomo eCommerce platform while there. I gained experience with the following technologies during my time at Namify AngularJS, .NET, and Microsoft SQL Server.</p>
-                        </div>
-                        <img src="https://www.registrationsaver.com/wp-content/uploads/2018/12/Race-Entry.png"/>
-                    </div>
-                    <div className="job-section__jobs__item">
-                        <div>
-                            <h5>Customer Service Representative</h5>
-                            <h6>Namify LLC</h6>
-                            <h6><i class="fas fa-calendar-day"></i>January 2018 to May 2018</h6>
-                            <p>I was a Frontend Web Developement Intern at Namify from January 2018 to May 2018. I worked on the Axomo eCommerce platform while there. I gained experience with the following technologies during my time at Namify AngularJS, .NET, and Microsoft SQL Server.</p>
-                        </div>
-                        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ec0429cf-1471-466c-b958-f3b1faa7896b/d5zfuqv-2100dad2-6615-4a54-970d-f2a452581111.png/v1/fill/w_893,h_895,strp/domino_icon_by_slamiticon_d5zfuqv-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNyIsInBhdGgiOiJcL2ZcL2VjMDQyOWNmLTE0NzEtNDY2Yy1iOTU4LWYzYjFmYWE3ODk2YlwvZDV6ZnVxdi0yMTAwZGFkMi02NjE1LTRhNTQtOTcwZC1mMmE0NTI1ODExMTEucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.ya_zbUNA04g8XGgO7-YGj5QYE23XVworgWfpg_VXD1o" />
-                    </div>
-                </div>
+                <p>Hover or tap on a job on the timeline to view a detailed description. References can be made available by request.</p>
                 <div className="job-timeline">
                     <div className="job-timeline__item">
                         <div className="job-timeline__item__circle">
-                            <img src="https://namify.com/wp-content/uploads/2017/06/Namify_Logo_Color-300x132.png"/>
+                            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ec0429cf-1471-466c-b958-f3b1faa7896b/d5zfuqv-2100dad2-6615-4a54-970d-f2a452581111.png/v1/fill/w_893,h_895,strp/domino_icon_by_slamiticon_d5zfuqv-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNyIsInBhdGgiOiJcL2ZcL2VjMDQyOWNmLTE0NzEtNDY2Yy1iOTU4LWYzYjFmYWE3ODk2YlwvZDV6ZnVxdi0yMTAwZGFkMi02NjE1LTRhNTQtOTcwZC1mMmE0NTI1ODExMTEucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.ya_zbUNA04g8XGgO7-YGj5QYE23XVworgWfpg_VXD1o"/>
                         </div>
                         <div className="job-timeline__item__label">
-                            <div>Namify LLC</div>
-                        </div>
-                        <div className="job-timeline__item__description">
-                            <div className="job-timeline__item__description__carot"></div>
-                            I was a Frontend Web Developement Intern at Namify from January 2018 to May 2018. I worked on the Axomo eCommerce platform while there. I gained experience with the following technologies during my time at Namify AngularJS, .NET, and Microsoft SQL Server.
-                        </div>
-                    </div>
-                    <div className="job-timeline__item">
-                        <div className="job-timeline__item__circle">
-                            <img src="https://namify.com/wp-content/uploads/2017/06/Namify_Logo_Color-300x132.png"/>
-                        </div>
-                        <div className="job-timeline__item__label">
-                            <div>Namify LLC</div>
-                        </div>
-                        <div className="job-timeline__item__description">
-                            <div className="job-timeline__item__description__carot"></div>
-                            I was a Frontend Web Developement Intern at Namify from January 2018 to May 2018. I worked on the Axomo eCommerce platform while there. I gained experience with the following technologies during my time at Namify AngularJS, .NET, and Microsoft SQL Server.
-                        </div>
-                    </div>
-                    <div className="job-timeline__item">
-                        <div className="job-timeline__item__circle">
-                            <img src="https://namify.com/wp-content/uploads/2017/06/Namify_Logo_Color-300x132.png"/>
-                        </div>
-                        <div className="job-timeline__item__label">
-                            <div>Namify LLC</div>
+                            <div>Dominos' Pizza</div>
+                            <div>Customer Service Representative</div>
                         </div>
                         <div className="job-timeline__item__tick-mark tick-right"></div>
+                        <div className="job-timeline__item__tick-label-right tick-right">Novemeber 2017</div>
+                        <div className="job-timeline__item__tick-mark tick-left"></div>
+                        <div className="job-timeline__item__tick-label-left tick-left">May 2017</div>
+                        <div className="job-timeline__item__description">
+                            <div className="job-timeline__item__description__carot"></div>
+                            I was a Frontend Web Developement Intern at Namify from January 2018 to May 2018. I worked on the Axomo eCommerce platform while there. I gained experience with the following technologies during my time at Namify AngularJS, .NET, and Microsoft SQL Server.
+                        </div>
+                    </div>
+                    <div className="job-timeline__item">
+                        <div className="job-timeline__item__circle">
+                            <img src="https://namify.com/wp-content/uploads/2017/06/Namify_Logo_Color-300x132.png"/>
+                        </div>
+                        <div className="job-timeline__item__label">
+                            <div>Namify LLC</div>
+                            <div>Front-End Web Development Intern</div>
+                        </div>
+                        <div className="job-timeline__item__tick-mark tick-right"></div>
+                        <div className="job-timeline__item__tick-label-right tick-right">May 2018</div>
+                        <div className="job-timeline__item__tick-mark tick-left"></div>
+                        <div className="job-timeline__item__tick-label-left tick-left">January 2018</div>
+                        <div className="job-timeline__item__description">
+                            <div className="job-timeline__item__description__carot"></div>
+                            I was a Frontend Web Developement Intern at Namify from January 2018 to May 2018. I worked on the Axomo eCommerce platform while there. I gained experience with the following technologies during my time at Namify AngularJS, .NET, and Microsoft SQL Server.
+                        </div>
+                    </div>
+                    <div className="job-timeline__item">
+                        <div className="job-timeline__item__circle">
+                            <img src="https://www.registrationsaver.com/wp-content/uploads/2018/12/Race-Entry.png"/>
+                        </div>
+                        <div className="job-timeline__item__label">
+                            <div>Race Entry</div>
+                            <div>QA Analyst</div>
+                        </div>
+                        <div className="job-timeline__item__tick-mark tick-right"></div>
+                        <div className="job-timeline__item__tick-label-right tick-right">Today</div>
+                        <div className="job-timeline__item__tick-mark tick-left"></div>
+                        <div className="job-timeline__item__tick-label-left tick-left">June 2018</div>
                         <div className="job-timeline__item__description">
                             <div className="job-timeline__item__description__carot"></div>
                             I was a Frontend Web Developement Intern at Namify from January 2018 to May 2018. I worked on the Axomo eCommerce platform while there. I gained experience with the following technologies during my time at Namify AngularJS, .NET, and Microsoft SQL Server.
