7775311ffbb01a479c5d8adda94b4aa88c6e3da4
Miguel Rust <hava12soft@gmail.com>
2019-07-10 22:58:30 -0600

    Added email field to contact form.

--- src/components/contact.js
+++ src/components/contact.js
@@ -18,7 +18,8 @@ export default class Contact extends Component {
             name: "",
             message: "",
             error: "",
-            success: false
+            success: false,
+            email: ""
         }
     }
 
@@ -41,39 +42,51 @@ export default class Contact extends Component {
         });
     }
 
+    updateEmail(e){
+        const email = e.target.value;
+        this.setState({
+            email
+        });
+    }
+
     sumbitForm(){
-        const {name, message} = this.state;
-        if(verifiedHuman && name != "" && message != ""){
+        const {name, message, email} = this.state;
+        if(verifiedHuman && name != "" && message != "" && email != "" && email.indexOf("@") != -1 && email.split("@")[1].indexOf(".") != -1){
             emailjs.send('gmail', 'template_ARqIPPjo', {...this.state});
             //window.open(`mailto:prettygoodstudiosapps@gmail.com?subject=${encodeURI(this.state.name)+encodeURI(" - Contact Form Request")}&body=${this.state.message}`);
             this.setState({
                 name: "",
                 message: "",
                 error: "",
+                email: "",
                 success: true
             });
-        }else if(name != "" && message != ""){
+        }else if(name != "" && message != "" && email != "" && email.indexOf("@") != -1 && email.split("@")[1].indexOf(".") != -1){
             this.setState({
                 error: "You must prove you are a human."
             });
-        }else if(name != ""){
+        }else if(name != "" && email != "" && email.indexOf("@") != -1 && email.split("@")[1].indexOf(".") != -1){
             this.setState({
                 error: "You must provide a message."
             });
-        }else{
+        }else if(email != "" && email.indexOf("@") != -1 && email.split("@")[1].indexOf(".") != -1){
             this.setState({
                 error: "You must provide a name/entity."
+            });
+        }else{
+            this.setState({
+                error: "You must provide a valid email."
             });
         }
     }
 
     render(){
-        const {name, message, error, success} = this.state;
+        const {name, message, error, success, email} = this.state;
         return(
             <div className="contact" id="contact">
                 <h1>Contact</h1>
                 <div className="contact__attention">
-                    <h1>Let's Talk</h1>
+                    <h1>Let's Work Together</h1>
                     <div>
                         <span>Phone: (385) 309-4385</span>
                         <span>Email: prettygoodstudiosapps@gmail.com</span>
@@ -81,8 +94,11 @@ export default class Contact extends Component {
                     </div>
                 </div>
                 <div className="contact__form">
+                    <h3>Contact Form</h3>
                     <label for="contactFormIdentity">Name/Entity</label>
                     <input type="text" name="contactFormIdentity" id="contactFormIdentity" value={name} onChange={(e) => this.updateName(e)}/>
+                    <label for="contactFormEmail">Email</label>
+                    <input type="email" name="contactFormEmail" id="contactFormEmail" value={email} onChange={(e) => this.updateEmail(e)}/>
                     <label for="contactFormMessage" name="contactFormMessage" id="contactFormMessage">Message</label>
                     <textarea name="contactFormMessage" id="contactFormMessage" rows="10" value={message} onChange={(e) => this.updateMessage(e)}></textarea>
                     <center><div id="captcha"></div></center>
