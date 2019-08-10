a473201833c86bfc70297d76ef02cc1967b1a6de
Miguel Rust <hava12soft@gmail.com>
2019-08-10 16:03:33 -0600

    Porfolio projects.

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,6 +1,9 @@
 import React, {Component} from "react";
 
 import geoImage from "../images/frame-001.jpg";
+import bioImage from "../images/geofoc.png";
+import oldSite from "../images/oldsite.png";
+import accuracy from "../images/accuracy.png";
 
 const PortfolioItem = (props) => {
     const {url, image, title, description} = props;
@@ -31,23 +34,23 @@ class Portfolio extends Component {
                     stack: "Rails"
                 },
                 {
-                    title: "GeoFocus",
-                    description: "Web app for photographers built with Ruby On Rails, React, Mapbox, AWS SES and AWS S3. It was deployed to the Heroku Paas.",
-                    image: geoImage,
-                    url: "https://geofoc.us",
+                    title: "Bioencounters",
+                    description: "Crowdsourcing platform for bioencounters built with Ruby on Rails.",
+                    image: bioImage,
+                    url: "https://bioencounters.geofoc.us",
                     stack: "Rails"
                 },{
-                    title: "GeoFocus",
-                    description: "Web app for photographers built with Ruby On Rails, React, Mapbox, AWS SES and AWS S3. It was deployed to the Heroku Paas.",
-                    image: geoImage,
-                    url: "https://geofoc.us",
+                    title: "Old Portfolio Site",
+                    description: "Site showcasing my skills as a web developer. The site includes a blog, about page, trending topics and portfolio page. It was built using Ruby on Rails",
+                    image: oldSite,
+                    url: "https://miguelrust.herokuapp.com",
                     stack: "Rails"
                 },{
-                    title: "GeoFocus",
-                    description: "Web app for photographers built with Ruby On Rails, React, Mapbox, AWS SES and AWS S3. It was deployed to the Heroku Paas.",
-                    image: geoImage,
-                    url: "https://geofoc.us",
-                    stack: "Rails"
+                    title: "Accuracy Accounting",
+                    description: "Marketing page for a mock accounting services company. The page was built with React and features adavanced features such as an HTML5 canvas animation and intercative calendar. The interactive calendar depends on a custom rest API that was built with Express, Nodejs and MySQL.",
+                    image: accuracy,
+                    url: "https://accuracy-accounting.herokuapp.com/",
+                    stack: "React"
                 },{
                     title: "GeoFocus",
                     description: "Web app for photographers built with Ruby On Rails, React, Mapbox, AWS SES and AWS S3. It was deployed to the Heroku Paas.",
