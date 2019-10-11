d59cc745a4dcdfaea51ee45559f14d1ef8233e8b
Miguel Rust <hava12soft@gmail.com>
2019-10-10 20:19:54 -0600

    Worth another shot.

--- webpack.config.js
+++ webpack.config.js
@@ -26,17 +26,16 @@ module.exports = {
                 loader: "babel-loader"
             }
         },
-        ,
-          {
-            test: /\.(ttf|eot|woff|woff2)$/,
-            use: {
-              loader: "file-loader",
-              options: {
-                name: "[name].[ext]",
-		            outputPath: "fonts"
-              },
+        {
+          test: /\.(ttf|eot|woff|woff2)$/,
+          use: {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts"
             },
           },
+        },
         {
             test: /\.scss$/,
             use: [
